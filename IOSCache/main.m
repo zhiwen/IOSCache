326ceedf1c7bbe897599d5e41f9af1fee7631d33
zhiwen.mizw <zhiwen.mizw@alibaba-inc.com>
2014-06-18 19:18:36 +0800

    ioscache-project

---
+++ IOSCache/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  IOSCache
+//
+//  Created by zhiwen.mizw on 6/18/14.
+//  Copyright (c) 2014 zhiwen.mizw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
